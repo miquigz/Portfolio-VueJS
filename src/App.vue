24cc3b55b0b898b902ecc86b8589d42ab7762678
Miqueas Gimenez <miquigz02@gmail.com.ar>
2024-11-03 12:50:39 -0300

    estructura del proyecto, cap 1 y 2

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+const nombreApellido= 'Juan Perez';
+</script>
+
+<template>
+ <header>
+  <h1> {{ nombreApellido }} </h1>
+  <p> Bienvenido a Vue 3 </p>
+ </header>
+</template>
+
+<style scoped>
+</style>
