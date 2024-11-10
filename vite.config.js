a27efb83aedd7a5b8226295b968fc225709cbf2d
miquigz <miquigz02@gmail.com>
2024-11-10 09:23:41 -0300

    baseurl githubpages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base:"https://miquigz.github.io/portfolio-vuejs-2024/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
